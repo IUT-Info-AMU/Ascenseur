0cc053b5cf08b97547c88cfadf5ee21781ef7f60
jeremtop <jeremtop@gmail.com>
2016-01-07 22:04:23 +0100

    test

---
+++ Ascenseur/src/ascenseur/Controleur.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ascenseur;
+
+/**
+ *
+ * @author jeremtop
+ */
+public class Controleur {
+   public void creerRequeteExterne(){
+       //todo
+   }
+   public void choisirAscenseur(){
+       //todo
+   }
+   
+}
