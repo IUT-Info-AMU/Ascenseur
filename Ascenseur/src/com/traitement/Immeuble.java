10b09c485cf766c6086b90015998772749ecdcff
moonlight83340 <moonlight83340@users.noreply.github.com>
2016-01-15 19:45:34 +0100

    javadoc

--- Ascenseur/src/com/traitement/Immeuble.java
+++ Ascenseur/src/com/traitement/Immeuble.java
@@ -14,27 +14,41 @@ import java.util.ArrayList;
  * 
  */
 public class Immeuble {
-    
     private int nombreEtage;
     private ArrayList<Ascenseur> ascenseurs;
-    
+    /*
+    *Constructeur de la classe Immeuble
+    *@param nombreEtage le nombre d'étage, un entier
+    */
     public Immeuble (int nombreEtage) {
         this.ascenseurs = new ArrayList<Ascenseur> ();
         this.nombreEtage = nombreEtage;
     }
     
+    /*
+    *Cette fonction renvoit le nombre d'étage de l'Immeuble
+    *@return int le nombre d'étage
+    */
      public int getNombreEtage(){
         
         return this.nombreEtage;
     }
     
+    /*
+    * Cette fonction renvoit un tableau contenant els ascenseur de l'Immeuble
+    *@return ArrayList<Ascenseur> un tableau d'ascenseurs
+    */
     public ArrayList<Ascenseur> getAscenseurs(){
         
         return this.ascenseurs;
     }
     
-    public void ajouterAscenseur (Ascenseur a) {
-        this.ascenseurs.add (a);
+    /*
+    *Ajoute un Ascenseur à l'Immeuble
+    *@param ascenseur un Ascenseur
+    */
+    public void ajouterAscenseur (Ascenseur ascenseur) {
+        this.ascenseurs.add (ascenseur);
     }
 
 }
