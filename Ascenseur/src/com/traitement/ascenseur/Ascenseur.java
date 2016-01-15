119453d0e80332c87b94c60039c2bc500567a7d4
moonlight83340 <moonlight83340@users.noreply.github.com>
2016-01-15 20:54:59 +0100

    javadoc et modification mineur

--- Ascenseur/src/com/traitement/ascenseur/Ascenseur.java
+++ Ascenseur/src/com/traitement/ascenseur/Ascenseur.java
@@ -10,20 +10,55 @@ import java.util.LinkedList;
 
 /**
  *
- * @author Bprog
+ * @author Bprog, Gaëtan (javadoc)
  */
 public abstract class Ascenseur extends AscenseurObservable{
     
+    /*
+    *Bloque les portes de l'ascenseur
+    *@param bloquer boolean
+    */
     abstract public void bloquer (boolean bloquer);
+    
+    /*
+    *Ouvre les portes de l'ascenseur
+    *@param ouvrir boolean
+    */
     abstract public void ouvrir (boolean ouvrir);
+    
+    /*
+    *MEt en mouvement l'ascenseur
+    *@param mouvement boolean
+    */
     abstract public void mettreEnMouvement (boolean mouvement);
     
+    /*
+    *Ajoute une Requete à l'ascenseur
+    *@param r Requete
+    */
     abstract public void ajouterRequete (Requete r);
+    
+    /*
+    *Creer une RequeteInterne
+    *@param numEtage int
+    */
     abstract public void creerRequeteInterne (int numEtage);
     
+    /*
+    *Retourne le numéro d'étage
+    *@return int numEtage
+    */
     abstract public int getNumEtage ();
+    
+    /*
+    *Retourne une List de Requete
+    *@return LinkedList<Requete>
+    */
     abstract public LinkedList<Requete> getRequetes ();
     
+    /*
+    *
+    */
     abstract public void action ();
     
 }
